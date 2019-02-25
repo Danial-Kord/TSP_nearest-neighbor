56c567c324ae203eec64ae5ce1b293ea0872dccb
Danial <danial.kordmodanlou@gmail.com>
2019-02-25 22:45:35 +0330

    calculating execution time plus showing points in a row in usage

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <sys/time.h>
+#include <time.h>
 using namespace std;
 #include <math.h>
+#include <chrono>
+using namespace std::chrono;
 typedef struct point{
     long double x;
     long double y;
     bool check;
     long double des(point in){
         long double output = pow(pow(x-in.x,2) + pow(y-in.y,2),0.5);
-        cout<<"output : "<<output<<endl;
         return output ;
     }
     void build(){
@@ -16,44 +19,71 @@ typedef struct point{
         check = false;
     }
 }Point;
+class Stopwatch
+{
+private:
+    steady_clock::time_point start;
 
+public:
+    Stopwatch(){
+        reset();
+    }
+
+    ~Stopwatch(){}
+
+    // Start the clock
+    void reset(){ start = steady_clock::now(); }
+
+    // Read time elapsed in nanoseconds
+    double getTime(){
+        return std::chrono::nanoseconds (steady_clock::now() - start).count();
+    }
+};
 int main() {
     int n = 0;
     cin >> n;
-    Point  points[n];
-    for (int i = 0; i <n ; ++i) {
-        cin >> points[i].x;
-        cin >> points[i].y;
+    Point points[n];
+    for (int i = 0; i < n; ++i) {
+//        cin >> points[i].x;
+//        cin >> points[i].y;
+        points[i].x = rand() % 20000;
+        points[i].y = rand() % 20000;
         points[i].check = false;
     }
-    Point *first_point = &points[rand()%n];
-    (*first_point).check = true;
-    Point *min;
-    long double final_res = 0;
-    Point *curent;
-    curent = first_point;
-    for (int k = 0; k < n; k++) {
-        if (!points[k].check) {
-            min = &points[k];
-            long double min_des = (*curent).des((*min));
-            for (int j = k+1; j < n; j++) {
-                if(!points[j].check){
-                    double new_des = (*curent).des(points[j]);
-                    if(min_des > new_des){
-                        min_des = new_des;
-                        min = &points[j];
+        timespec ts;
+        // clock_gettime(CLOCK_MONOTONIC, &ts); // Works on FreeBSD
 
+        Point *first_point = &points[rand() % n];
+        (*first_point).check = true;
+        Point *min;
+        long double final_res = 0;
+        Point *curent;
+    Stopwatch sw;
+        curent = first_point;
+        cout << "points : [ x : " << first_point->x << " y : " << first_point->y << ",";
+        for (int k = 0; k < n; k++) {  //O(n+1)
+            if (!points[k].check) { //O(n)
+                min = &points[k]; //O(n)
+                long double min_des = (*curent).des((*min)); //O(n)
+                for (int j = k + 1; j < n; j++) { //O(n^2 + n*(n-1) + .... + n) = O(n^2))
+                    if (!points[j].check) { //O(n^2 + n*(n-1) + .... + 0) = O(n^2))
+                        double new_des = (*curent).des(points[j]);//O(n^2 + n*(n-1) + .... + 0) = O(n^2))
+                        if (min_des > new_des) {//O(n^2 + n*(n-1) + .... + 0) = O(n^2))
+                            min_des = new_des;//O(n^2 + n*(n-1) + .... + 0) = O(n^2))
+                            min = &points[j];//O(n^2 + n*(n-1) + .... + 0) = O(n^2))
+                        }
                     }
                 }
+                final_res += min_des;
+                (*min).check = true;
+                curent = min;
+                cout << "x : " << curent->x << " y : " << curent->y << ",";
+                k = -1;
             }
-            final_res += min_des;
-            cout << "+=  "<<min_des << endl;
-            (*min).check = true;
-            curent = min;
-            k = -1;
         }
+        cout << " x : " << first_point->x << " y : " << first_point->y << "]";
+        final_res += (*first_point).des((*curent));
+        cout << endl << "final result : " << final_res;
+    cout<<endl<< "#####Execution time: " <<sw.getTime() <<" nanoseconds" <<endl;
+        return 0;
     }
-    final_res += (*first_point).des((*curent));
-    cout << final_res;
-    return 0;
-}
